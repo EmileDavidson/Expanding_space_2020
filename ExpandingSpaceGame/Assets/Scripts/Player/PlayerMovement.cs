89b53d44abf17aa9dd9406a0fa17ae3d93db8a67
EmileDavidson <Emiledavidson395@gmail.com>
2020-05-07 11:33:29 +0200

    Simple Playermovement & Cam toegevoegd

---
+++ ExpandingSpaceGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public bool canmove = true;
+    public float speed = 6f;
+    public float gravity = 20f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    public float jumpspeed = 8f;
+    public int maxDubbleJumps = 0;
+    public int jumps;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        move();
+    }
+
+    public void move()
+    {
+        if (canmove == false) { return; }
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                moveDirection.y = jumpspeed;
+            }
+            jumps = 0;
+        }
+        else
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), moveDirection.y, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection.x *= speed;
+            moveDirection.z *= speed;
+            if (Input.GetKeyDown(KeyCode.Space) && jumps < maxDubbleJumps)
+            {
+                moveDirection.y = jumpspeed;
+                jumps++;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ ExpandingSpaceGame/Assets/Scripts/Player/PlayerRotationMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotationMovement : MonoBehaviour
+{
+    public bool canmousemove = true;
+    private CharacterController controller;
+    public float speedY = 1f;
+    public float speedX = 1f;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        MouseMovement();
+    }
+
+    public void MouseMovement()
+    {
+        if (canmousemove == false) { return; }
+        yaw += speedX * Input.GetAxis("Mouse X");
+        pitch -= speedY * Input.GetAxis("Mouse Y");
+        pitch = Mathf.Clamp(pitch, -20f, 28f);
+        transform.eulerAngles = new Vector3(0, yaw, 0);
+    }
+}
