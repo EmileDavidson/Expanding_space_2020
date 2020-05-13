c1d33fb7a2032860a49b0f4524ffa8019f1c1748
Zakaria-ElGhoul <30044@ma-web.nl>
2020-05-13 11:07:30 +0200

    Loading Screen en UI toegevoegd

---
+++ ExpandingSpaceGame/Assets/Scripts/UI/UI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public int sceneIndex;
+    public Text ui_Text;
+
+    //When the mouse hovers over the GameObject, it turns to this color (red)
+    Color m_MouseOverColor = Color.gray;
+
+    //This stores the GameObject’s original color
+    Color m_OriginalColor;
+
+    //Get the GameObject’s mesh renderer to access the GameObject’s material and color
+    MeshRenderer m_Renderer;
+
+
+    void Start()
+    {
+        //Fetch the mesh renderer component from the GameObject
+        m_Renderer = GetComponent<MeshRenderer>();
+        //Fetch the original color of the GameObject
+        m_OriginalColor = m_Renderer.material.color;
+        ui_Text.enabled = false;
+    }
+
+
+    void OnMouseOver()
+    {
+        // Change the color of the GameObject to red when the mouse is over GameObject
+        m_Renderer.material.color = m_MouseOverColor;
+        ui_Text.enabled = true;
+
+    }
+
+
+    void OnMouseExit()
+    {
+        // Reset the color of the GameObject back to normal
+        m_Renderer.material.color = m_OriginalColor;
+        ui_Text.enabled = false;
+    }
+
+
+    void OnMouseDown()
+    {
+
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
